202186ff1fddcce7c79c51dcae9e3b3de0ea3b67
Yudai Tanaka <ytnk531@gmail.com>
2021-04-05 05:42:04 +0900

    Add output from lambda function feature.

--- lib/csv_serializer/methods.rb
+++ lib/csv_serializer/methods.rb
@@ -1,20 +1,48 @@
 # frozen_string_literal: true
+require 'csv'
 
 module CsvSerializer
   module Method
     extend ActiveSupport::Concern
 
     class_methods do
-      def to_csv(*columns)
-        columns = columns.presence || attribute_names
-        require 'csv'
+      def to_csv(*columns, **definitions)
+        if definitions.present?
+          from_functions definitions
+        else
+          from_columns columns
+        end
+      end
 
-        CSV.generate do |csv|
-          header = columns
-          csv << header
-          all.pluck(*columns).each do
-            csv << (_1.is_a?(Array) ? _1 : [_1])
+      def from_functions(functions)
+        generate_csv(functions.keys, functions) do |row|
+          functions.values.map do |function|
+            function.call row
           end
+        end
+      end
+
+      def from_columns(columns)
+        columns = columns.presence || attribute_names
+        generate_csv(columns, columns) do
+          (_1.is_a?(Array) ? _1 : [_1])
+        end
+      end
+
+      def generate_csv(header, columns_or_functions)
+        CSV.generate do |csv|
+          csv << header
+          data_source(columns_or_functions).each do |row|
+            csv << yield(row)
+          end
+        end
+      end
+
+      def data_source(functions_or_columns)
+        if functions_or_columns.is_a?(Array)
+          all.pluck(*functions_or_columns)
+        else
+          all
         end
       end
     end
--- test/csv_serializer_test.rb
+++ test/csv_serializer_test.rb
@@ -1,13 +1,17 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class CsvSerializerTest < ActiveSupport::TestCase
-  test "it has a version number" do
+  test 'it has a version number' do
     assert CsvSerializer::VERSION
   end
 
   test 'it outputs csv' do
-    Person.create!(id: 1, name: 'sample1', tall: 128, weight: 34, created_at: '2020-1-1 10:02:39', updated_at: '2020-1-1 11:02:39',)
-    Person.create!(id: 2, name: 'sample2', tall: 130, weight: 32, created_at: '2020-1-1 12:02:39', updated_at: '2020-1-1 14:02:39',)
+    Person.create!(id: 1, name: 'sample1', tall: 128, weight: 34, created_at: '2020-1-1 10:02:39',
+                   updated_at: '2020-1-1 11:02:39')
+    Person.create!(id: 2, name: 'sample2', tall: 130, weight: 32, created_at: '2020-1-1 12:02:39',
+                   updated_at: '2020-1-1 14:02:39')
 
     expected = <<~CSV
       id,name,age,tall,weight,created_at,updated_at
@@ -27,6 +31,28 @@ class CsvSerializerTest < ActiveSupport::TestCase
       sample2
     CSV
     assert_equal expected, Person.all.to_csv(:name)
+  end
+
+  test 'it outputs from lambda function' do
+    Person.create!(id: 1, name: 'sample1')
+    Person.create!(id: 2, name: 'sample2')
+
+    expected = <<~CSV
+      long name
+      sample1sample1
+      sample2sample2
+    CSV
+    assert_equal expected, Person.all.to_csv("long name": ->(user) { user.name * 2 })
+
+    expected = <<~CSV
+      long name,short
+      sample1sample1,e1
+      sample2sample2,e2
+    CSV
+    assert_equal expected, Person.all.to_csv(
+      "long name": ->(user) { user.name * 2 },
+      "short": ->(user) { user.name[-2..] }
+    )
   end
 
   test 'it outputs csv2' do
